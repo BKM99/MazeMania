e44df97604ab2b3d197a525214e0c83d39115a99
Brandon Morimoto <bmorimoto99@gmail.com>
2020-11-14 18:11:52 -0800

    created Maze 3

--- src/model/Maze3.java
+++ src/model/Maze3.java
@@ -1,4 +1,54 @@
 package model;
-
+/**
+ * Constructs the third maze
+ */
 public class Maze3 extends Maze {
+
+    public Maze3() {
+        super();
+        //ROW 0
+        addWall(0,2);
+        addWall(0,3);
+        addWall(0,4);
+        addWall(0,5);
+        addWall(0,6);
+        addWall(0,7);
+
+        //ROW 1
+        addWall(1,0);
+        addWall(1,7);
+
+        //ROW 2
+        addWall(2,0);
+        addWall(2,2);
+        addWall(2,3);
+        addWall(2,5);
+        addWall(2,6);
+        addWall(2,7);
+
+        //ROW 3
+        addWall(3,0);
+        addWall(3,3);
+
+        //ROW 4
+        addWall(4,0);
+        addWall(4,3);
+
+        //ROW 5
+        addWall(5,0);
+        addWall(5,2);
+        addWall(5,3);
+        addWall(5,4);
+        addWall(5,5);
+        addWall(5,7);
+
+        //ROW 6
+        addWall(6,6);
+        addWall(6,7);
+
+        //ROW 7
+        addWall(7,2);
+        addWall(7,4);
+
+    }
 }
