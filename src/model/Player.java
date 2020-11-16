17f47f79a190700a77096b7bb72fcd8c07edbe5d
tmtrinidad <0trinidad6@gmail.com>
2020-11-15 21:25:49 -0800

    Updated player move methods

--- src/model/Player.java
+++ src/model/Player.java
@@ -54,41 +54,41 @@ public class Player {
     }
 
     /**
-     * Moves the player up by 1 if there is a path
+     * Moves the player up by 1 if there is no wall
      */
     public void moveUp() {
-        //Checks to maze sure player is not at the very top and that there is a path above
-        if (rowPosition > 0 && maze.isPath(rowPosition - 1, colPosition)) {
+        //Checks to maze sure player is not at the very top and that there is not a wall above
+        if (rowPosition > 0 && !maze.isWall(rowPosition - 1, colPosition)) {
             rowPosition--;
         }
     }
 
     /**
-     * Moves the player down by 1 if there is a path
+     * Moves the player down by 1 if there is no wall
      */
     public void moveDown() {
-        //Checks to maze sure player is not at the very bottom and that there is a path below
-        if (rowPosition < 7 && maze.isPath(rowPosition + 1, colPosition)) {
+        //Checks to maze sure player is not at the very bottom and that there is not a wall below
+        if (rowPosition < 7 && !maze.isWall(rowPosition + 1, colPosition)) {
             rowPosition++;
         }
     }
 
     /**
-     * Moves the player right by 1 if there is a path
+     * Moves the player right by 1 if there is no wall
      */
     public void moveRight() {
-        //Checks to maze sure player is not at the very right and that there is a path to the right
-        if (colPosition < 7 && maze.isPath(rowPosition, colPosition + 1)) {
+        //Checks to maze sure player is not at the very right and that there is not a wall to the right
+        if (colPosition < 7 && !maze.isWall(rowPosition, colPosition + 1)) {
             colPosition++;
         }
     }
 
     /**
-     * Moves the player left by 1 if there is a path
+     * Moves the player left by 1 if there is no wall
      */
     public void moveLeft() {
-        //Checks to maze sure player is not at the very left and that there is a path to the left
-        if (colPosition > 0 && maze.isPath(rowPosition, colPosition - 1)) {
+        //Checks to maze sure player is not at the very left and that there is not a wall to the left
+        if (colPosition > 0 && !maze.isWall(rowPosition, colPosition - 1)) {
             colPosition--;
         }
     }
