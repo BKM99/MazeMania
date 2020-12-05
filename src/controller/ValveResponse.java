b943dce8e96439ea151c3d0c98fdf335cc5873c0
tmtrinidad <0trinidad6@gmail.com>
2020-12-05 15:20:02 -0800

    Move Message / Enum Javadoc

--- src/controller/ValveResponse.java
+++ src/controller/ValveResponse.java
@@ -1,8 +1,20 @@
 package controller;
 
+/**
+ * Represent valve state
+ */
 public enum ValveResponse {
+    /**
+     * Valve has missed
+     */
     MISS,
+    /**
+     * Valve has been executed
+     */
     EXECUTED,
+    /**
+     * Valve is finished
+     */
     FINISH
 }
 
